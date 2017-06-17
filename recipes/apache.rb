fe848e3d2dec523d682bbc727b48630222ac026e
Gray <gray@live.co.uk>
2017-06-17 18:44:23 +0100

    Adds recipe for installing apache2

---
+++ recipes/apache.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook:: gravcms
+# Recipe:: apache
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+# Install httpd/Apache package
+
+if node['platform_family'] == 'rhel'
+    package = 'httpd'
+elsif node['platform_family'] == 'debian'
+    package = 'apache2'
+end
+
+package 'apache2' do
+    package_name package
+    action :install
+end
+
+service 'apache2' do
+    service_name 'httpd'
+    action [:start, :enable]
+end
+ 
